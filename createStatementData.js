b3b3da85c513192c5276af23e5f0216972455838
Nicolas Ponce de Leon <nicolaspdle@gmail.com>
2019-03-06 22:37:28 -0400

    REFACTOR: create createStatementData.js

---
+++ createStatementData.js
@@ -0,0 +1,60 @@
+import * as PLAYS from './plays.json';
+import * as INVOICE from './invoices.json';
+
+export default function createStatementData() {
+  const statementData = {};
+  statementData.customer = INVOICE.customer;
+  statementData.performances = INVOICE.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  return statementData;
+}
+
+function enrichPerformance(aPerformance) {
+  const result = Object.assign({}, aPerformance);
+  result.play = playFor(result);
+  result.amount = amountFor(result);
+  result.volumeCredits = volumeCreditsFor(result);
+  return result;
+}
+
+function totalAmount(data) {
+  return data.performances.reduce((total, p) => total + p.amount, 0);
+}
+
+function totalVolumeCredits(data) {
+  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+}
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === aPerformance.play.type)
+  result += Math.floor(aPerformance.audience / 5);
+  return result;
+}
+
+function amountFor(aPerformance) {
+  let result = 0;
+  switch (aPerformance.play.type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${aPerformance.play.type}`);
+  }
+  return result;
+}
+
+function playFor(aPerformance) {
+ return PLAYS[aPerformance.playID];
+}
