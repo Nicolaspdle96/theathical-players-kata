1959c86843d41f365d49596b1bfafbb74ed47126
Nicolas Ponce de Leon <nicolaspdle@gmail.com>
2019-03-07 09:16:48 -0400

    REFACTOR: add PerformanceCalculator

--- createStatementData.js
+++ createStatementData.js
@@ -11,11 +11,16 @@ export default function createStatementData() {
 }
 
 function enrichPerformance(aPerformance) {
+  const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
   const result = Object.assign({}, aPerformance);
-  result.play = playFor(result);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
+  result.play = calculator.play;
+  result.amount = calculator.getAmount();
+  result.volumeCredits = calculator.getVolumeCredits();
   return result;
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+ return new PerformanceCalculator(aPerformance, aPlay);
 }
 
 function totalAmount(data) {
@@ -25,36 +30,47 @@ function totalAmount(data) {
 function totalVolumeCredits(data) {
   return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
 }
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === aPerformance.play.type)
-  result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
 
 function amountFor(aPerformance) {
-  let result = 0;
-  switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${aPerformance.play.type}`);
-  }
-  return result;
+    return new PerformanceCalculator(aPerformance,playFor(aPerformance)).getAmount();
 }
 
 function playFor(aPerformance) {
  return PLAYS[aPerformance.playID];
+}
+
+class PerformanceCalculator {
+ constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+ }
+
+ getAmount(){
+    let result = 0;
+    switch (this.play.type) {
+        case "tragedy":
+            result = 40000;
+            if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+            }
+        break;
+            case "comedy":
+            result = 30000;
+            if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+            }
+            result += 300 * this.performance.audience;
+        break;
+        default:
+            throw new Error(`unknown type: ${this.play.type}`);
+        }
+    return result;
+ }
+ getVolumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.play.type) result +=
+   Math.floor(this.performance.audience / 5);
+    return result;
+   }
 }
