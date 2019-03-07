a2f839a901f679f4cdb636e7be419123ca98f028
Nicolas Ponce de Leon <nicolaspdle@gmail.com>
2019-03-06 21:14:51 -0400

    REFACTOR: extract function appleSauce

--- statement.js
+++ statement.js
@@ -1,17 +1,29 @@
 function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf, plays);
     // print line for this order
-      result += ` ${playFor(perf, plays).name}: ${usd(amountFor(perf, plays))} (${perf.audience} seats)\n`;
-      totalAmount += amountFor(perf, plays);
+    result += ` ${playFor(perf, plays).name}: ${usd(amountFor(perf, plays))} (${perf.audience} seats)\n`;
   }
+  let totalAmount = appleSauce(invoice, plays);
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits(invoice, plays)} credits\n`;
   return result;
-  
+}
+
+function appleSauce(invoice, plays) {
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
+    totalAmount += amountFor(perf, plays);
+  }
+  return totalAmount;
+}
+
+function totalVolumeCredits(invoice, plays) {
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf, plays);
+  }
+  return volumeCredits;
 }
 
 function usd(aNumber) {
